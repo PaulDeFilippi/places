5aaec7fe7d2c96e0e81f0f430fa7df1a16c56b6c
Paul Defilippi <allinfo.pd@gmail.com>
2019-07-19 19:17:16 -0400

    Initial Commit

---
+++ places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  places
+//
+//  Created by Paul Defilippi on 7/19/19.
+//  Copyright © 2019 Paul Defilippi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
